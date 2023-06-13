410dc1f3126015ed87ef95e3cfc3c6b78466fed4
abhishekkumarec <126902053+abhishekkumarec@users.noreply.github.com>
2023-06-13 18:10:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import SideNav from './SideNav';
 import MainSection from './MainSection';
 import Header from './Header';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/App.css';
 const App = () => {
+  const [active, setActive] = useState("INBOX");
 
 
   return (
@@ -12,10 +12,10 @@ const App = () => {
     <Header />
       <div className='row'>
         <div className='col-md-2'>
-          <SideNav change={}/>
+          <SideNav change={setActive}/>
         </div>
         <div className='col-md-10'>
-          <MainSection active = {}/>
+          <MainSection active = {active}/>
         </div>
       </div>
     </div>
